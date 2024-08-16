7ef084a7521ed1d2b4b3974bffd289ff353910dc
Govind Dandekar <govind@Govinds-Mac-mini.local>
2024-08-16 15:13:39 -0700

    Add ref to input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  // create ref value to get value entered into input field
+  const playerName = useRef();
+  
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value)
+    // clear input after submitted
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+          ref={playerName}
+          type="text"
+        />
+        <button onClick={handleClick}>
+          Set Name
+        </button>
       </p>
     </section>
   );
